91238b74ff60b79ffaa2136f9cd912d59378b938
Luis Orozco <35777412+luis01orozco@users.noreply.github.com>
2020-03-29 18:38:03 -0500

    Update mode_shutdown.cpp

--- ArduCopter/mode_shutdown.cpp
+++ ArduCopter/mode_shutdown.cpp
@@ -4,11 +4,9 @@
 ** Author: Luis Orozco
 ** File: overlay.py
 ** Description: 
-   File responsible for holding functions necessary for overlaying textual information
-   on video frames taken by the camera and saving videos onboard the raspberry pi
+   Flight mode responsible for stopping the motors. Exact copy of mode_brake.cpp
+   except different name, mode_shutdown.cpp
  */
-
-
 #include "Copter.h"
 
 
@@ -28,44 +26,101 @@
  */
 bool ModeShutdown::init(bool ignore_checks)
 {
-    // if (!ignore_checks) {
-        // if (!AP::ahrs().home_is_set()) {
-        //     return false;
-        // }
-    // }
+    // set target to current position
+    init_target();
 
-    // initialise waypoint and spline controller
-    // wp_nav->wp_and_spline_init();
-    // _state = RTL_Starting;
-    // _state_complete = true; // see run() method below
-    // terrain_following_allowed = !copter.failsafe.terrain;
+    // initialize vertical speed and acceleration
+    pos_control->set_max_speed_z(BRAKE_MODE_SPEED_Z, BRAKE_MODE_SPEED_Z);
+    pos_control->set_max_accel_z(BRAKE_MODE_DECEL_RATE);
+
+    // initialise position and desired velocity
+    if (!pos_control->is_active_z()) {
+        pos_control->set_alt_target_to_current_alt();
+        pos_control->set_desired_velocity_z(inertial_nav.get_velocity_z());
+    }
+
+    _timeout_ms = 0;
 
     return true;
 }
 
 
-/*
+/* 
 ** Name: run
 
-** Description: recevies commands from transmitter, pilot's inputs, and converts
-    them to motor values
+** Description: is an exact copy of brake
 
-** Parameters: none
+** Parameters:
+    N/A
 
-** Return: none
+** Return:
+    N/A
 
 ** Notes:
-    stabilize_run - runs the main stabilize controller
-    should be called at 100hz or more
  */
 void ModeShutdown::run()
 {
+    // if not armed set throttle to zero and exit immediately
+    if (is_disarmed_or_landed()) {
+        make_safe_spool_down();
+        init_target();
+        return;
+    }
+
+    // set motors to full range
+    motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
+
+    // relax stop target if we might be landed
+    if (copter.ap.land_complete_maybe) {
+        loiter_nav->soften_for_landing();
+    }
+
+    // use position controller to stop
+    pos_control->set_desired_velocity_xy(0.0f, 0.0f);
+    pos_control->update_xy_controller();
 
     // call attitude controller
-    attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(0, 0, 0);
+    attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(wp_nav->get_roll(), wp_nav->get_pitch(), 0.0f);
 
-    // output pilot's throttle
-    attitude_control->set_throttle_out(0,
-                                       true,
-                                       g.throttle_filt);
+    // update altitude target and call position controller
+    // protects heli's from inflight motor interlock disable
+    if (motors->get_desired_spool_state() == AP_Motors::DesiredSpoolState::GROUND_IDLE && !copter.ap.land_complete) {
+        pos_control->set_alt_target_from_climb_rate(-abs(g.land_speed), G_Dt, false);
+    } else {
+        pos_control->set_alt_target_from_climb_rate_ff(0.0f, G_Dt, false);
+    }
+    pos_control->update_z_controller();
+
+    if (_timeout_ms != 0 && millis()-_timeout_start >= _timeout_ms) {
+        if (!copter.set_mode(Mode::Number::LOITER, ModeReason::BRAKE_TIMEOUT)) {
+            copter.set_mode(Mode::Number::ALT_HOLD, ModeReason::BRAKE_TIMEOUT);
+        }
+    }
 }
+
+
+void ModeShutdown::timeout_to_loiter_ms(uint32_t timeout_ms)
+{
+    _timeout_start = millis();
+    _timeout_ms = timeout_ms;
+}
+
+
+void ModeShutdown::init_target()
+{
+    // initialise position controller
+    pos_control->set_desired_velocity_xy(0.0f,0.0f);
+    pos_control->set_desired_accel_xy(0.0f,0.0f);
+    pos_control->init_xy_controller();
+
+    // initialise pos controller speed and acceleration
+    pos_control->set_max_speed_xy(inertial_nav.get_velocity().length());
+    pos_control->set_max_accel_xy(BRAKE_MODE_DECEL_RATE);
+    pos_control->calc_leash_length_xy();
+
+    // set target position
+    Vector3f stopping_point;
+    pos_control->get_stopping_point_xy(stopping_point);
+    pos_control->set_xy_target(stopping_point.x, stopping_point.y);
+}
+
